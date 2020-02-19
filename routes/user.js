ee49b2d10bb689d2f3e1ce841837cf1e2555042b
habib-chawki <touati.habib.chawki@gmail.com>
2020-02-19 20:45:05 +0100

    Add authentication, validation  and encryption

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,39 @@
-const userModel = require('../models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
+const bcrypt = require('bcrypt');
 
+const userModel = require('../models/user');
+const { isInputValid } = require('../src/utils');
+
+const router = express.Router();
 router.use(bodyParser.json());
+
 // get user information
 router.get('/', (req, res) => {
    res.status(200).send(req.body);
 });
 
 // add a new user
-router.post('/', (req, res) => {
-   userModel
-      .create({
-         name: req.body.name,
-         email: req.body.email,
-         password: req.body.password
-      })
-      .then(user => res.status(200).send('user added!'))
-      .catch(err => res.status(400).send('could not add user!'));
+router.post('/', async (req, res) => {
+   const { name, email, password } = req.body;
+   const salt = 8;
+
+   // check input validity
+   if (isInputValid(req.body)) {
+      try {
+         const user = await userModel.create({
+            name: name.trim(),
+            email,
+            password: await bcrypt.hash(password, salt)
+         });
+
+         res.status(201).send(`User created successfuly ! ${user}`);
+      } catch (e) {
+         res.status(400).send(`Error: could not create user ! ${e.message}`);
+      }
+   } else {
+      res.status(400).send('Invalid input !');
+   }
 });
 
 module.exports = router;
