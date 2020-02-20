90d7cffb7f63104720a366e265aa2a628dbb9e1a
habib-chawki <touati.habib.chawki@gmail.com>
2020-02-20 22:42:37 +0100

    Enhance validation with mongoose

--- models/user.js
+++ models/user.js
@@ -1,9 +1,32 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = mongoose.Schema({
-   name: { type: String, required: true },
-   email: { type: String, required: true },
-   password: { type: String, required: true }
+   name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 30,
+      match: /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/i
+   },
+   email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+         if (!validator.isEmail(value)) {
+            throw new Error('Invalid Email !');
+         }
+      }
+   },
+   password: {
+      type: String,
+      required: true,
+      minlength: 5
+   }
 });
 
 module.exports = mongoose.model('user', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
 const userModel = require('../models/user');
-const { isInputValid } = require('../src/utils');
 
 const router = express.Router();
 router.use(bodyParser.json());
@@ -18,21 +17,16 @@ router.post('/', async (req, res) => {
    const { name, email, password } = req.body;
    const salt = 8;
 
-   // check input validity
-   if (isInputValid(req.body)) {
-      try {
-         const user = await userModel.create({
-            name: name.trim(),
-            email,
-            password: await bcrypt.hash(password, salt)
-         });
+   try {
+      const user = await userModel.create({
+         name,
+         email,
+         password: await bcrypt.hash(password, salt)
+      });
 
-         res.status(201).send(`User created successfuly ! ${user}`);
-      } catch (e) {
-         res.status(400).send(`Error: could not create user ! ${e.message}`);
-      }
-   } else {
-      res.status(400).send('Invalid input !');
+      res.status(201).send(`User created successfuly: ${user}`);
+   } catch (e) {
+      res.status(400).send(`Error: could not create user: ${e.message}`);
    }
 });
 
