566f3febdff3451ca36cb6fe9546db76b2732733
habib-chawki <touati.habib.chawki@gmail.com>
2020-03-09 17:24:20 +0100

    Feat: comment on posts

--- routes/comment.js
+++ routes/comment.js
@@ -1,14 +1,42 @@
 const express = require('express');
+const validator = require('validator');
 
 const postModel = require('../models/post');
 const auth = require('../utils/authentication');
 
 const router = express.Router();
+router.use(auth);
 
-// TODO: create a comment
+// create a comment
+router.post('/', async (req, res) => {
+   try {
+      // validate post id
+      if (!validator.isMongoId(req.body.postId)) {
+         throw new Error('Invalid post id.');
+      }
 
-// TODO: edit a comment
+      // find which post the comment belongs to
+      const post = await postModel.findById(req.body.postId);
 
-// TODO: delete a comment
+      console.log(req.body.comment);
+      // check if post exists and add comment to post's comments list
+      if (post) {
+         post.comments.push(req.body.comment);
+         await post.save();
+         return res
+            .status(200)
+            .send(`Comment added successfuly to post: ${post.content}`);
+      }
 
+      throw new Error('Error adding comment.');
+   } catch (e) {
+      res.status(404).send(e.message);
+   }
+});
+
+// edit a comment by id
+router.put('/:id', (req, res) => {});
+
+// delete a comment by id
+router.delete('/:id', (req, res) => {});
 module.exports = router;
