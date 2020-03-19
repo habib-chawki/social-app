6a7bfd1fb229f97da0ea9c9bb3db1036c94cc0d2
habib-chawki <touati.habib.chawki@gmail.com>
2020-03-19 16:42:15 +0100

    Remove all users ahead of testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
-const app = require('../src/app');
 
-test('Should sign up user and return token', async () => {
-   await request(app)
+const app = require('../src/app');
+const User = require('../models/user');
+
+beforeEach(async () => {
+   // delete all users before testing
+   await User.deleteMany({});
+});
+
+// test sign up route
+test('Should sign up user successfuly', async () => {
+   const user = await request(app)
       .post('/user/signup')
       .send({
-         email: 'habib2@test.com',
+         email: 'habib@test.com',
          password: 'thisismypass'
       })
       .expect(201);
