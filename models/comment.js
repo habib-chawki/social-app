b4c438eeac5ec28cb4d8d3e5d865af2af5195bb2
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-29 17:37:41 +0200

    Refactor: comment model

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = mongoose.Schema({
    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
-   content: String,
+   content: { type: String, required: true, trim: true },
 });
 
 module.export = mongoose.model('Comment', commentSchema);
