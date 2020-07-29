d4522f81694c8f6a1441c0109aae870467527205
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-29 17:38:03 +0200

    Fix: create comment

--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,8 @@ const express = require('express');
 const validator = require('validator');
 
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 const auth = require('../utils/auth');
 const editComment = require('../utils/edit-comment');
 
@@ -12,23 +14,35 @@ router.use(auth);
 
 // create a comment
 router.post('/', async (req, res) => {
+   // retrive post id from query string
+   const { postId } = req.query.postId;
+
    try {
       // validate post id
-      if (!validator.isMongoId(req.body.postId)) {
+      if (!validator.isMongoId(postId)) {
          throw new Error('Invalid post id.');
       }
 
-      // find which post the comment belongs to
-      const post = await Post.findById(req.body.postId);
+      // create comment
+      const comment = await Comment.create({
+         owner: req.user._id,
+         post: postId,
+         content: req.body.content,
+      });
 
-      // check if post exists and add comment to post's comments list
-      if (post) {
-         post.comments.push({ content: req.body.content, owner: req.user._id });
-         await post.save();
+      return res.status(201).send(comment);
 
-         // return the newly added comment (the last one in the comments list)
-         return res.status(201).send(post.comments[post.comments.length - 1]);
-      }
+      // // find which post the comment belongs to
+      // const post = await Post.findById(postId);
+
+      // // check if post exists and add comment to post's comments list
+      // if (post) {
+      //    post.comments.push({ content: req.body.content, owner: req.user._id });
+      //    await post.save();
+
+      //    // return the newly added comment (the last one in the comments list)
+      //    return res.status(201).send(post.comments[post.comments.length - 1]);
+      // }
 
       throw new Error('Error adding comment.');
    } catch (e) {
