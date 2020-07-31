e9a562ff05438d55709a3505fd4e6413323a329b
habib-chawki <touati.habib.chawki@gmail.com>
2020-07-31 12:37:11 +0200

    Setup profile schema

---
+++ schema/profile.js
@@ -0,0 +1,106 @@
+const mongoose = require('mongoose');
+
+const profileSchema = mongoose.Schema(
+   {
+      owner: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User',
+         required: true,
+      },
+      avatar: {
+         type: Buffer,
+         default: undefined,
+      },
+      firstName: {
+         type: String,
+         minLength: 3,
+         maxLength: 30,
+         trim: true,
+         default: '',
+      },
+      middleName: {
+         type: String,
+         minLength: 3,
+         maxLength: 30,
+         trim: true,
+         default: '',
+      },
+      lastName: {
+         type: String,
+         minLength: 3,
+         maxLength: 30,
+         trim: true,
+         default: '',
+      },
+      address: { type: String, default: '' },
+      birthday: { type: Date, default: Date.now() },
+      gender: { type: String, default: 'Unknown' },
+      bio: {
+         type: String,
+         maxLength: 100,
+         default: '',
+      },
+      experience: [
+         {
+            _id: false,
+            startDate: {
+               type: Date,
+               required: true,
+            },
+            endDate: Date,
+            current: {
+               type: Boolean,
+               default: false,
+            },
+            position: {
+               type: String,
+               required: true,
+            },
+            company: {
+               type: String,
+               required: true,
+            },
+            description: {
+               type: String,
+               maxLength: 200,
+            },
+         },
+      ],
+      education: [
+         {
+            _id: false,
+            startDate: {
+               type: Date,
+               required: true,
+            },
+            endDate: Date,
+            current: {
+               type: Boolean,
+               default: false,
+            },
+            major: {
+               type: String,
+               required: true,
+            },
+            school: {
+               type: String,
+               required: true,
+            },
+            description: {
+               type: String,
+               maxLength: 200,
+            },
+         },
+      ],
+      skills: {
+         technical: [String],
+         organizational: [String],
+      },
+      languages: [String],
+   },
+   {
+      timestamps: true,
+   }
+);
+
+module.exports = profileSchema;
