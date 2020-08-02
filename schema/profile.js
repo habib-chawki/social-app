89858d74fde0ce15eb111f1992a4bf8d3556ca13
habib-chawki <touati.habib.chawki@gmail.com>
2020-08-02 15:42:11 +0200

    Fix: remove owner

--- schema/profile.js
+++ schema/profile.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 
 const profileSchema = mongoose.Schema(
    {
-      owner: {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: 'User',
-         required: true,
-      },
       avatar: {
          type: Buffer,
          default: undefined,
