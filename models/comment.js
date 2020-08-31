70b9573b3a5c76c2d631047ad89e14f17c594496
habib-chawki <touati.habib.chawki@gmail.com>
2020-08-31 14:33:30 +0200

    Add comment to post comments list

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Post = require('./post');
 
 const commentSchema = mongoose.Schema(
    {
@@ -10,5 +11,12 @@ const commentSchema = mongoose.Schema(
       timestamps: true,
    }
 );
+
+commentSchema.post('save', async function () {
+   // add comment to appropriate post
+   const post = await Post.findById(this.post);
+   post.comments.push(this._id);
+   await post.save();
+});
 
 module.exports = mongoose.model('Comment', commentSchema);
