75cc656ca3d1ba7ecf7410d8b091c0bf03725c57
habib-chawki <touati.habib.chawki@gmail.com>
2020-09-20 14:53:05 +0200

    Handle errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,3 +1,17 @@
-function errorHandler(err, req, res, next) {}
+// error handling middleware
+function errorHandler(err, req, res, next) {
+   // create error object
+   const error = {
+      status: err.status,
+      message: err.message,
+      path: req.originalUrl,
+      timestamp: Date.now(),
+   };
 
-module.exports = errorHanlder;
+   //send back error along with status code
+   res.status(err.statusCode).send(error);
+
+   next();
+}
+
+module.exports = errorHandler;
