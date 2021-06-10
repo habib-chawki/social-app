5c4a435be315e21497064830a0c2fde123dd9c90
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-10 14:36:47 +0200

    Create comment

--- services/commentService.js
+++ services/commentService.js
@@ -3,6 +3,22 @@ const httpError = require('http-errors');
 const Comment = require('../models/comment');
 const logger = require('../utils/logger');
 
-async function createComment(owner, post, content) {}
+async function createComment(owner, post, content) {
+   try {
+      const comment = await Comment.create({ owner, post, content });
+
+      if (comment) {
+         logger.info(
+            'Comment created ' + JSON.stringify({ owner, post, content })
+         );
+         return comment;
+      }
+
+      throw new Error('Create comment failed');
+   } catch (err) {
+      logger.error('Create comment failed ' + err);
+      throw httpError(500, 'Create comment failed');
+   }
+}
 
 module.exports = { createComment };
