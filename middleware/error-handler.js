3c01ec70fa6a15222eb139f62a4c5235c231a7cb
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-11 15:27:54 +0200

    Set default error message and add error logging

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,17 @@
+const logger = require('../utils/logger');
+
 // error handling middleware
 function errorHandler(err, req, res, next) {
    // create error object
    const error = {
       status: err.status || 500,
-      message: err.message,
+      message: err.message || 'Something went wrong',
       path: req.originalUrl,
       timestamp: Date.now(),
    };
 
    //send back error along with status code
+   logger.error('Http error ' + JSON.stringify(error));
    res.status(err.statusCode).send(error);
 
    next();
