55e03736c51feec7423438f3d5fbd50c5f709f2a
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-11 14:57:33 +0200

    Get user profile

--- services/profile-service.js
+++ services/profile-service.js
@@ -1,5 +1,23 @@
 const User = require('../models/user');
+const logger = require('../utils/logger');
 
-async function getProfile() {}
+async function getProfile(userId) {
+   try {
+      const user = await User.findById(userId);
+
+      if (user.profile) {
+         logger.info(
+            'Fetched profile ' +
+               JSON.stringify({ userId, profile: user.profile })
+         );
+         return user.profile;
+      }
+
+      throw new Error('Profile not found ' + JSON.stringify({ userId }));
+   } catch (err) {
+      logger.error('Profile not found ' + err);
+      throw httpError(404, 'Profile not found');
+   }
+}
 
 module.exports = { getProfile };
