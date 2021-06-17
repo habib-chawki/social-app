93ccda7917308504ac7a2b743ee84bcb7a53fbce
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-17 16:17:33 +0200

    Refactor unit test, create post

--- tests/unit/post-service.test.js
+++ tests/unit/post-service.test.js
@@ -3,22 +3,22 @@ const Post = require('../../models/post');
 
 jest.mock('../../models/post');
 
-fit('should create post', async () => {
-   const userId = '507f1f77bcf86cd799439011';
-   const postContent = 'This is a post';
+const userId = '507f1f77bcf86cd799439011';
+const postContent = 'This is a post';
 
-   const post = {
-      owner: userId,
-      content: postContent,
-      comments: [],
-   };
+const post = {
+   owner: userId,
+   content: postContent,
+   comments: [],
+};
 
+fit('should create post', () => {
    // given the post model create method response
    Post.create.mockResolvedValue(post);
 
    // when createPost is invoked
-   const createdPost = await postService.createPost(userId, postContent);
-
-   // then expect the post to have been created
-   expect(createdPost).toBe(post);
+   postService.createPost(userId, postContent).then((createdPost) => {
+      // then expect the post to have been created
+      expect(createdPost).toBe(post);
+   });
 });
