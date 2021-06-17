2a5143ce3471142c72009b8e69162a379466269b
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-17 16:32:32 +0200

    Test create post, use async await

--- tests/unit/post-service.test.js
+++ tests/unit/post-service.test.js
@@ -10,21 +10,24 @@ const post = {
    comments: [],
 };
 
-fit('should create post', () => {
+fit('should create post', async () => {
    // given the post model create method response
    Post.create.mockResolvedValue(post);
 
-   // when createPost is invoked
-   postService.createPost(post.owner, post.content).then((createdPost) => {
-      // then expect the post to have been created
-      expect(createdPost).toBe(post);
-   });
+   // when createPost() is invoked
+   const createdPost = await postService.createPost(post.owner, post.content);
+
+   // then expect the post to have been created
+   expect(createdPost).toBe(post);
 });
 
 fit('should get post by id', async () => {
    // given the get post
    Post.findById.mockResolvedValue(post);
 
+   // when a request to get the post by id is made
    const fetchedPost = await postService.getPostById(post._id);
+
+   // then expect the post to have been fetched successfully
    expect(fetchedPost).toBe(post);
 });
