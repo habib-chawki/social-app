f106f9cc29ffd0b4e677f00358a80474956f80e0
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-26 14:24:05 +0200

    Test: create comment

---
+++ tests/unit/comment-service.test.js
@@ -0,0 +1,9 @@
+const commentService = require('../../services/comment-service');
+const Comment = require('../../models/comment');
+
+const comment = {
+   _id: '507f1f77bcf86cd799439010',
+   owner: '507f1f77bcf86cd799439020',
+   post: '507f1f77bcf86cd799439030',
+   content: 'This is a comment',
+};
