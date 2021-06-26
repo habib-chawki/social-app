679eb07ec343920d53263b66b7f3814be6a508dc
habib-chawki <touati.habib.chawki@gmail.com>
2021-06-26 14:27:13 +0200

    Test: create comment

--- tests/unit/comment-service.test.js
+++ tests/unit/comment-service.test.js
@@ -7,3 +7,15 @@ const comment = {
    post: '507f1f77bcf86cd799439030',
    content: 'This is a comment',
 };
+
+it('should create comment', async () => {
+   Comment.create = jest.fn().mockReturnValue(comment);
+
+   const response = await commentService.createComment(
+      comment.owner,
+      comment.post,
+      comment.content
+   );
+
+   expect(response).toEqual(comment);
+});
